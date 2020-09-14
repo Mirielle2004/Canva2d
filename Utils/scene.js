bef875ef8f18f93a17f621658aea2b81abc82ec9
CodeBreaker <47723333+Mirielle2004@users.noreply.github.com>
2020-09-14 11:39:04 -0700

    Update scene.js

--- Utils/scene.js
+++ Utils/scene.js
@@ -1,44 +1,51 @@
 /**
- * @description Pricipal class that manipulates the canvas elements. The Scene
- * defines the properties for the viewport of the graphics
+ * @description principal class for the scene rendering
+ * 
  */
 class Scene {
+
     /**
      * @constructor
-     * @param {Object} arg keyword arguement for the scene
+     * @param {Object} param0 setup for the scene
      * 
      */
-    constructor(arg) {
-        this.canvas = arg.canvas || null;
+    constructor({canvas, width, height, backgroundColor, controls, backgroundImage}) {
+        this.canvas = canvas;
         try {
-            this.ctx = this.canvas.getContext("2d");
+            this.getContext = () => this.canvas.getContext("2d");;
         } catch(error) {
             throw new Error("Failed to initialize canvas: " + error.message);
         }
-        this.width = arg.width || 300;
-        this.height = arg.height || 300;
+        this.width = width || 300;
+        this.height = height || 300;
         this.canvas.width = this.width;
         this.canvas.height = this.height;
 
         // controls binded directly to the scene
-        this.controls = arg.controls || [];
+        this.controls = controls || [];
         this.joystick = null;
         this.swipe = null;
 
         this.controls.forEach((c, i) => {
             if(c.event === "swipe") {
-                this.swipe = new SwipeControl(this.canvas, c.type);
+                this.swipe = new Swipe({element:this.canvas, type:c.type});
             } else if(c.event === "joystick") {
-                this.joystick = new Joystick(this.canvas, c.style);
+                this.joystick = new JoyStick({canvas:this.canvas, ...c.style});
             }
         });
         
         // set default backgroundColor
-        if(arg.backgroundColor !== undefined)
-            this.canvas.style.backgroundColor = arg.backgroundColor;
+        if(backgroundColor !== undefined)
+            this.canvas.style.backgroundColor = backgroundColor;
 
+        if(backgroundImage !== undefined) {
+            this.canvas.style.backgroundImage = `url(${backgroundImage})`;
+            this.canvas.backgroundSize = "cover";
+        }
+
+        // animations
         this.startLoop = callback => requestAnimationFrame(callback);
-        this.elapsedTime = 0;   // frames per second
+        this.elapsedTime = 0;   // elapsed time till the scene is active
         this.animationFrame = null;
 
         // fps
@@ -46,16 +53,25 @@ class Scene {
         this.timeStarted = null;
         this.fps = null;
 
+        Scene.setScene(this);
         Scene.setContext(this.canvas);
     }
 
+    /**
+     * @description calculate fps for the current scene
+     * 
+     */
     calcFps() {
         this.timeEnded = performance.now();
         this.fps = 1000 / (this.timeEnded - this.timeStarted);
         this.timeStarted = this.timeEnded;
     }
 
-    // 
+    /**
+     * @description start the main animation's loop
+     * @param {Function} callback function's to be updated
+     * 
+     */
     mainLoop(callback) {
         const animate = elapsedTime => {
             this.elapsedTime = elapsedTime;
@@ -66,12 +82,23 @@ class Scene {
         this.startLoop(animate);
     }
 
-
+    /**
+     * @description set the current 2D rendering context to an instacen of canvas
+     * @param {HTMLCanvasElement} canvas a reference to the canvas
+     * 
+     */
     static setContext(canvas) {
         try {
             CURRENT_CONTEXT = canvas.getContext("2d");
         } catch (err) {
-            throw new Error(`Failed to set the global variabl "CURRENT_CONTEXT": ${err.message}`);
+            throw new Error(`Failed to set the global variable "CURRENT_CONTEXT": ${err.message}`);
         }
+    }
+
+
+    static setScene(scene) {
+        if(scene instanceof Scene)
+            CURRENT_SCENE = scene;
+        else throw TypeError(`"${scene}" must be an instance of Scene`);
     }
 }
