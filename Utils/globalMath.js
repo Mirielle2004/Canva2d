bf0f9d900a385c2ac0ba69a77d981c7e8c82d16b
CodeBreaker <47723333+Mirielle2004@users.noreply.github.com>
2020-09-14 11:37:52 -0700

    Update globalMath.js

--- Utils/globalMath.js
+++ Utils/globalMath.js
@@ -1,9 +1,46 @@
 Math.__proto__ = {
-    
-    getPolarCoord(angle = 0, magnitude = 0) {
-        let x = Math.cos(angle) * magnitude;
-        let y = Math.sin(angle) * magnitude;
-        return {x, y};
+
+    /**
+     * 
+     * @description Get random numbers within a specified range
+     * @param {Number} min Minimum value's range
+     * @param {Number} max Maximum value's range
+     * @returns {Numer} Number between min and max
+     * 
+     */
+    randRange(min=0, max=1) {
+        return this.random() * (max - min + 1) + min;
+    },
+
+    /**
+     * 
+     * @description Get random items from an array
+     * @param {Array} arr Array to get random items from.
+     * @returns {Any} Random items from an array
+     * 
+     */
+    randFromArray(arr) {
+        return arr[this.random() * arr.length | 0];
+    },
+
+    /**
+     * 
+     * @description Converts a given number to radian
+     * @param {Number} n Number to be converted into radian
+     * @returns {Number} Argument in radian
+     * 
+     */
+    toRadian(n) {
+        return n * this.PI / 180;
+    },
+
+    /**
+     * @description Converts a given number to degree
+     * @param {Number} value Number to be converted into degree
+     * @returns {Number} Argument in degree.
+     */
+    toDegree(value) {
+        return value * 180 / this.PI;
     }
 
 }
