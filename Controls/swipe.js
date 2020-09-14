cdb2a97ec530bc40f7609b91fccaf446e1357a42
CodeBreaker <47723333+Mirielle2004@users.noreply.github.com>
2020-09-14 11:23:17 -0700

    Update swipe.js

--- Controls/swipe.js
+++ Controls/swipe.js
@@ -1,24 +1,21 @@
 /**
- * 
  * @description gives a swipe gesture functionality to it's binded element
- * 
  */
-class SwipeControl {
+class Swipe {
+
     /**
-     * 
      * @constructor
      * @param {Object} element elements to add the listener to
      * 
      */
-    constructor(element, type="default", debug=false) {
+    constructor({element, type="default"}) {
         this.element = element;             
         this.direction = null;              // direction of the swipe
-        this.isActive = false;              // flag if swipe is active
+        this.isActive = false;              
         this.pos = new Vector(0, 0);        // position of the cursor on start
 
         if(type !== undefined) {
             switch(type.toLowerCase()) {
-
                 case "touch":
                     this.touch();
                     break;
@@ -28,22 +25,23 @@ class SwipeControl {
                     break;
     
                 case "default":
-                    this.mouse();
-                    this.touch();
+                    if ("ontouchstart" in window)
+                        this.touch();
+                    else if("onmousedown" in window)
+                        this.mouse();
                     break;
                 default:
                     throw new Error(`Swipe of type "${type}" does not exist`);
             };
         } else {
-            this.mouse();
-            this.touch();
+            if ("ontouchstart" in window)
+                this.touch();
+            else if("onmousedown" in window)
+                this.mouse();
         }
-
-        this.debug = debug;
     }
 
     /**
-     * 
      * @description check for swipes
      * @param {vector} startPoint starting vector when element is focused
      * @param {vector} endPoint current point during swipe
@@ -69,7 +67,6 @@ class SwipeControl {
         this.element.addEventListener("touchstart", e => {
             this.isActive = true;
             this.pos = new Vector(e.touches[0].pageX, e.touches[0].pageY);
-            this.checkDebug(console.log, `Swipe Starts...`);
         });
 
         this.element.addEventListener("touchmove", e => {
@@ -85,14 +82,12 @@ class SwipeControl {
                     direction: this.direction
                 }
             }));
-            this.checkDebug(console.log, `${this.direction.toUpperCase()} Swipe`);
             e.preventDefault();
         });
 
         this.element.addEventListener("touchend", () => {
             this.isActive = false;
             this.pos = new Vector(0, 0);
-            this.checkDebug(console.log, `Swipe ends...`);
         });
     }
 
@@ -103,7 +98,6 @@ class SwipeControl {
         this.element.addEventListener("mousedown", e => {
             this.isActive = true;
             this.pos = new Vector(e.clientX, e.clientY);
-            this.checkDebug(console.log, `Swipe Starts.....`);
         });
 
         this.element.addEventListener("mousemove", e => {
@@ -120,20 +114,13 @@ class SwipeControl {
                         direction: this.direction
                     }
                 }));
-
-                this.checkDebug(console.log, `${this.direction.toUpperCase()} Swipe`);
             }
         });
 
         this.element.addEventListener("mouseup", () => {
             this.isActive = false;
             this.pos = new Vector(0, 0);
-            this.checkDebug(console.log, `Swipe Ends...`);
         });
     }
      
 }
-
-
-
-Object.assign(SwipeControl.prototype, AbstractBaseMixin);
