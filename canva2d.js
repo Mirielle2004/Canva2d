0b6141e95998cded3b714473210b293d6dd7b554
Mirielle2004 <ccosmos409@gmail.com>
2020-12-18 14:22:55 -0800

    fix canva2d

--- canva2d.js
+++ canva2d.js
@@ -3,7 +3,7 @@
  * @author Mirielle S.
  * @name: Canva2D.js
  * @description A simple HTML5 Canvas game Engine
- * Last Revision: 4th Dec. 2020
+ * Last Revision: 18th Dec. 2020
  * 
  * 
  * MIT License 
@@ -35,7 +35,7 @@ const Canva2D = (function() {
         API: {
             author: "CodeBreaker",
             date: "18th Dec. 2020",
-            version: "v0.0.11-alpha",
+            version: "v0.0.13-alpha",
         },
         getAPI() {
             return this.API;
@@ -501,7 +501,6 @@ Canva2D.API.Vector2 = class Vector2 {
     scale(s) {
         return new Vector2(this.x * s, this.y * s);
     }
-<<<<<<< HEAD
 
     /**
      * @method addScale
@@ -513,52 +512,6 @@ Canva2D.API.Vector2 = class Vector2 {
     addScale(vec, s) {
         let v = Vector2.createFrom(vec);
         return new Vector2(this.x + v.x * s, this.y + v.y * s);
-=======
-};const Launcher = function(config) {
-    
-    let element = config.element === undefined ?document.createElement("CANVAS") : config.element;
-    if(config.element === undefined) {
-        element.width = config.width || 300;
-        element.height = config.height || 300;
-        element.style.backgroundColor = config.theme === "dark" ? "#222" : "#fff";
-    };
-    element.id = "launcher";
-    element.style.position = "absolute";
-    element.style.zIndex = "2000";
-    document.body.insertBefore(element, document.body.childNodes[0]);
-    
-    let fontSize = config.fontSize || 35;
-    let fontFamily = config.fontFamily || "Verdana";
-    
-    if(config.element === undefined) {
-        let ctx = element.getContext("2d");
-        // draw logo
-        ctx.beginPath();
-        ctx.moveTo(config.width/2, config.height/2 - 20);
-        for(let i=0; i <= 360; i+=60) {
-            let angle = i * Math.PI / 180;
-            let radius = fontSize * 3;
-            let x = config.width/2 + Math.cos(angle) * radius;
-            let y = (config.height/2 - 20) + Math.sin(angle) * radius;
-            ctx.lineTo(x, y);
-        }
-        ctx.fillStyle = config.theme === "dark" ? "#333" : "dimgray";
-        ctx.fill();            
-        // bat text
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.font = `bold ${fontSize}px ${fontFamily}`;
-        ctx.fillStyle = config.theme === "dark" ? "dimgray" : "#fff";
-        ctx.fillText("Canva2D", config.width/2, config.height/2 - 20);
-        // bat description
-        ctx.font = `bold ${fontSize - (fontSize-10)}px ${fontFamily}`;
-        ctx.fillStyle = "red";
-        ctx.fillText("Games API for web developers", config.width/2, config.height/2 + 20);
-        // copyright
-        ctx.font = `bold 10px ${fontFamily}`;
-        ctx.fillStyle = config.theme === "dark" ? "#fff" : "#222";
-        ctx.fillText("Mirielle "+new Date().getFullYear(), config.width/2, config.height - 50);
->>>>>>> 6f4afc42c2f7e5b80cf59b2074cacc193d2fd66c
     }
 
     /**
@@ -2606,8 +2559,4 @@ Canva2D.API.Scene = (function(width, height, dynamic=false) {
 
     }
 
-<<<<<<< HEAD
 }; // SCOPING ENDS;
-=======
-}
->>>>>>> 6f4afc42c2f7e5b80cf59b2074cacc193d2fd66c
