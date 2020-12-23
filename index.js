ccc7ceea6f56678452479614e90c881a9fd975af
Mirielle2004 <ccosmos409@gmail.com>
2020-12-22 18:21:49 -0800

    added main

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-let g = Canva2D.getAPI();
-
-
-
 const main = () => {
-    let l = g.Launcher();
-    l.then(e => console.log(e));
+
 };
 
-addEventListener("load", main);
+addEventListener('load', main);
