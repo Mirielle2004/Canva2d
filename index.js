72e9c2d772544b0f7c515bb81d6d930f1f2f17f1
Mirielle2004 <ccosmos409@gmail.com>
2021-01-12 21:19:31 -0800

    update API

--- index.js
+++ index.js
@@ -1,5 +1,159 @@
-const main = () => {
+let g, ctx, graph;
 
+const {E:nat, log, sinh, sin} = Math;
+/**
+ * Edit by Adding/Removing functions to this variable to modify the graph.
+ * NOTE: this function must return a value and note it's syntax
+ * Also note that this code computes functions from the smallest -X to X
+ */
+const functions = {
+    sinHFunc: x => [sinh(x * 0.05) * 50, "teal"],
+    logEFunc: x => [log(x) * 50, "green"],
+    sinFunc: x => [Math.sin(x * nat) * 50, "navy"],
 };
 
-addEventListener('load', main);
+
+const createGraph = (function(boxSize) {
+    let [W, H] = [innerWidth, innerHeight];
+    let graph = {};
+    graph.tileSize = boxSize;
+    graph.subTileLength = 10;
+    graph.subTileSize = ~~(graph.tileSize / graph.subTileLength);
+    graph.subTileDimension = new g.Vector2(10);
+
+    let colX = ~~(W / graph.tileSize);
+    let colY = ~~(H / graph.tileSize);
+
+    graph.column = !(colX % 2) ? colX : colX - 1;
+    graph.row = !(colY % 2) ? colY : colY - 1;
+    graph.dimension = new g.Vector2(graph.column, graph.row);
+    graph.size = graph.dimension.scale(graph.tileSize);
+    graph.offSet = new g.Vector2(W, H).sub(graph.size);
+
+    graph.color = {
+        origin: "red",
+        subTile: "red",
+        background:"#222",
+    };
+
+    graph.middle = graph.size.scale(0.5);
+
+    graph.drawLine = (ctx, tPos, pos, color, width=1) => {
+        tPos = new g.Vector2(tPos);
+        pos = new g.Vector2(pos);
+        ctx.save();
+        ctx.lineWidth = width;
+        ctx.beginPath();
+        ctx.moveTo(tPos.x, tPos.y);
+        ctx.lineTo(tPos.x + pos.x, tPos.y + pos.y);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+        ctx.restore();
+    };
+
+    graph.coord = function(pos) {
+        pos = new g.Vector2(300, 20);
+        let x = this.middle.x + pos.x;
+        let y = this.middle.y + pos.y;
+        return {x, y};
+    };
+
+    graph.drawFunction = function()  {
+        for(const func in functions) {
+            for(let i=-this.middle.x, c=0; i < this.middle.x; i++, c++) {
+                let call = functions[func](i);
+                let x = i;
+                let y = call[0];
+                let color = call[1];
+                let pos = new g.Vector2(x+this.middle.x, y + this.middle.y);
+                if(c < 1) {
+                    ctx.beginPath();
+                    ctx.moveTo(pos.x, pos.y);
+                } else 
+                    ctx.lineTo(pos.x, pos.y);
+                ctx.strokeStyle = color;
+                ctx.stroke();
+            };
+        };
+    };
+
+    graph.drawBackground = function(ctx) {
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = this.color["subTile"];
+        for(let row=0; row < this.row; row++) {
+            for(let col=0; col < this.column; col++) {
+                let pos = new g.Vector2(col, row).scale(this.tileSize);
+                ctx.strokeRect(pos.x, pos.y, this.tileSize, this.tileSize);
+                ctx.save();
+                ctx.lineWidth = 1;
+                for(let row=0; row < this.subTileLength; row++) {
+                    for(let col=0; col < this.subTileLength; col++) {
+                        let subPos = pos.add(new g.Vector2(col, row).scale(this.subTileSize));
+                        ctx.strokeRect(subPos.x, subPos.y, this.subTileSize, this.subTileSize);
+                    };
+                };
+                ctx.restore();
+            };
+        };
+    };
+
+    return graph;
+});
+
+const drawLine = (ctx, tPos, pos, color, width=1) => {
+    tPos = new g.Vector2(tPos);
+    pos = new g.Vector2(pos);
+    ctx.save();
+    ctx.lineWidth = width;
+    ctx.beginPath();
+    ctx.moveTo(tPos.x, tPos.y);
+    ctx.lineTo(tPos.x + pos.x, tPos.y + pos.y);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.restore();
+};
+
+
+const text = (txt, pos) => {
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = "20px Verdana";
+    ctx.fillText(txt, pos.x, pos.y);
+};
+
+function graphLoop() {
+    ctx = this.getContext("2d");
+    graph.drawFunction();
+};
+
+
+function graphBackgroundFunc() {
+    let ctx = this.getContext("2d");
+    graph.drawBackground(ctx);
+    drawLine(ctx, [graph.middle.x, 0], [0, graph.size.y], 
+        graph.color["origin"], 4);
+    drawLine(ctx, [0, graph.size.y/2], [graph.size.x, 0], 
+        graph.color["origin"], 4);
+};
+
+
+const main = () => {
+    g = Canva2D.getAPI();
+    graph = createGraph(50);
+    const [W, H] = [graph.size.x, graph.size.y];
+    let styles = {
+        position: "absolute",
+        left: `${innerWidth/2 - W/2}px`,
+        top: `${innerHeight/2 - H/2}px`
+    };
+    let backgroundScene = g.Scene(W, H, false);
+    backgroundScene.update = graphBackgroundFunc;
+    backgroundScene.css(styles);
+    backgroundScene.play();
+    let scene = g.Scene(W, H, false);
+    scene.css(styles);
+    scene.update = graphLoop;
+    scene.play();
+};
+
+addEventListener("load", main);
--- tmp.js
+++ tmp.js
@@ -1,113 +1,41 @@
-let canvas, gl, W, H;
-let startAnime = true;
+let g, ctx, map, mapFunc, camera;
 
-let vShader = `
-attribute vec4 a_position;
-attribute vec4 color;
-varying highp vec4 vColor;
-uniform mat4 viewMat;
-uniform mat4 rotMat;
-uniform mat4 projMat;
-void main() {
-    gl_Position = viewMat * projMat* (rotMat * a_position);
-    gl_PointSize = 10.0;
-    vColor = color;
-}`;
 
-let fShader = `
-varying highp vec4 vColor;
-void main() {
-    gl_FragColor = vColor;
-}`;
-
-const initShader = (vs, fs) => {
-    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(vertexShader, vs);
-    gl.shaderSource(fragmentShader, fs);
-    gl.compileShader(vertexShader);
-    gl.compileShader(fragmentShader);
-    let program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    gl.useProgram(program);
-    gl.program = program;
+function gameLoop() {
+    ctx = this.getContext("2d");
+    ctx.clearRect(0, 0, this.getWidth(), this.getHeight());
+    
+    camera.lookAt(map);
+    map.render(0, 3);
 };
 
-const createVertexBuffer = (data) => {
-    let buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-    return buffer;
+
+function drawWorld() {
+    let pos = this.index.mult(this.size);
+    if(this.id === 0) {
+        let pos = this.index.mult(this.size);
+        ctx.fillStyle = "navy";
+        ctx.fillRect(pos.x, pos.y, this.size.x, this.size.y);
+    } else if(this.id === 1) {
+        ctx.fillStyle ="green";
+        ctx.fillRect(pos.x, pos.y, this.size.x, this.size.y);
+        ctx.strokeStyle ="#222";
+        ctx.strokeRect(pos.x, pos.y, this.size.x, this.size.y);
+    };
 };
 
-let camera = new Vector3(), lookDir, yaw=0;
-let aa=0;
-
-const update = () => {
-    gl.enable(gl.DEPTH_TEST);
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    let up = new Vector3(0, 1, 0);
-    let target = new Vector3(0, 0, 1);
-    lookDir = Mat4x4.makeRotateY(yaw).multiplyVec(target.toArray());
-    target = camera.add(lookDir);
-    let lookAt = Mat4x4.setView(camera, target, up);
-
-    let rotMatr = Mat4x4.makeRotateZ(aa);
-
-    let projMatrice = new Matrix4();
-    projMatrice.setPerspective(30, W/H, 1, 1000);
-
-    // aa+=.01;
-    initShader(vShader, fShader);
-    let projMat = gl.getUniformLocation(gl.program, "projMat");
-    gl.uniformMatrix4fv(projMat, false, projMatrice.elements);
-    let viewMat = gl.getUniformLocation(gl.program, "viewMat");
-    gl.uniformMatrix4fv(viewMat, false, lookAt.items);
-    let rotMat = gl.getUniformLocation(gl.program, "rotMat");
-    gl.uniformMatrix4fv(rotMat, false, rotMatr.items);
-    let points = new Float32Array([
-        0.75, 1.0, -4.0, 1.0, 0.0, 0.0, 1.0,
-        0.25, -1.0, -4.0, 1.0, 0.0, 0.0, 1.0,
-        1.25, -1.0, -4.0, 1.0, 0.0, 0.0, 1.0,
-
-        0.75, 1.0, -2.0, 1.0, 1.0, 1.0, 1.0,
-        0.25, -1.0, -2.0, 1.0, 1.0, 1.0, 1.0,
-        1.25, -1.0, -2.0, 1.0, 1.0, 1.0, 1.0,
-    ]);
-    let vSize = points.BYTES_PER_ELEMENT;
-    let pointBuffer = createVertexBuffer(points);
-    let a_position = gl.getAttribLocation(gl.program, "a_position");
-    gl.vertexAttribPointer(a_position, 3, gl.FLOAT, false, vSize * 7, 0);
-    gl.enableVertexAttribArray(a_position);
-    let color = gl.getAttribLocation(gl.program, "color");
-    gl.vertexAttribPointer(color, 4, gl.FLOAT, false, vSize * 7, vSize * 3);
-    gl.enableVertexAttribArray(color);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-};
-
-const animate = () => {
-    update();
-    requestAnimationFrame(animate);
-};
-
-addEventListener("keydown", e => {
-    if(e.keyCode === 37) camera.x-=.1;
-    else if(e.keyCode === 39) camera.x += .1;
-    else if(e.keyCode === 38) camera.z -= .1;
-    else if(e.keyCode === 40) camera.z += .1;
-    else if(e.keyCode === 65) yaw += .1;
-});
 
 const main = () => {
-    canvas = document.getElementById("cvs");
-    W = canvas.width = innerWidth;
-    H = canvas.height = innerHeight;
-    gl = canvas.getContext("experimental-webgl");
-    startAnime ? requestAnimationFrame(animate) : update();
+    g = Canva2D.getAPI();
+    let scene = g.Scene(innerWidth, innerHeight, true);
+    scene.update = gameLoop;
+
+    map = g.TileMap(g.ORTHORGONAL, levelMap, [40, 15], [64, 64]);
+    map.draw = drawWorld;
+
+    camera = g.Camera(g.ORTHORGONAL, 0, 200);
+
+    scene.play();
 };
 
 addEventListener("load", main);
